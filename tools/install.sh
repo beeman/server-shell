1447e67efbe62b811280dc2c1c976db918ef42e4
Bram Borggreve <borggreve@gmail.com>
2020-02-02 15:20:29 -0500

    fix: prevent running zsh on an interactive shell

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,5 @@
 # Install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+RUNZSH=no sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
