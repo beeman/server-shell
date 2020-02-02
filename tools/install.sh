d211087a837375fae64e2c9a5e9a8bbe4c4d15b8
Bram Borggreve <borggreve@gmail.com>
2020-02-02 03:08:56 -0500

    fix: use correct repo for fetching .zshrc

--- tools/install.sh
+++ tools/install.sh
@@ -3,4 +3,4 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # Install zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # Add custom config
-curl -L https://raw.github.com/ohmyzsh/ohmyzsh/master/profile/.zshrc > ~/.zshrc
+curl -L https://raw.github.com/beeman/server-shell/master/profile/.zshrc > ~/.zshrc
